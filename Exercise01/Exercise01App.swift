77a4178774ba7b5ed3403650b5cabf13488557d6
Mattia Fasoli <fasoxy@gmail.com>
2022-05-09 16:03:57 +0200

    Initial Commit

---
+++ Exercise01/Exercise01App.swift
@@ -0,0 +1,17 @@
+//
+//  Exercise01App.swift
+//  Exercise01
+//
+//  Created by Mattia Fasoli on 09/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Exercise01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
